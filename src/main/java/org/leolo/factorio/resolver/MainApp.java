198a07c7344180d2bb484d376ad2874849e56db4
LO Kam Tao Leo <leolo@leolo.org>
2023-05-07 20:11:45 +0100

    Project Files

---
+++ src/main/java/org/leolo/factorio/resolver/MainApp.java
@@ -0,0 +1,11 @@
+package org.leolo.factorio.resolver;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MainApp {
+    public static void main(String[] args) {
+        Logger log = LoggerFactory.getLogger(MainApp.class);
+        log.debug("Started");
+    }
+}
